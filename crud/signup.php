16eac0af368026a32f3cf87377ec9ad0fd15fccd
unknown <faizanirshad260@gmail.com>
2025-02-17 23:59:53 -0800

    db connection

---
+++ crud/signup.php
@@ -0,0 +1,25 @@
+<?php
+include("query.php")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-4.6.2-dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container p-5 mt-5">
+<form action="" method="post">
+<div class="form-group">
+  <label for="">Name</label>
+  <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+  <small id="helpId" class="text-muted">Help text</small>
+</div>
+</form>
+    </div>
+    
+</body>
+</html>
